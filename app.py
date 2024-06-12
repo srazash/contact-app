f09d3e0ca7f9ae90fbaff212f768667d5ced7792
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-06-12 21:37:56 +0100

    contacts/ route working

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
     return redirect("/contacts")
 
 @app.route("/contacts")
-def contscts():
+def contacts():
     search = request.args.get("q")
     if search is not None:
         contacts_set = Contact.search(search)
--- contact.py
+++ contact.py
@@ -1,1 +1,13 @@
-    
+class Contact:
+    db = {}
+
+    def __init__(self, id_=None, first_=None, last_=None, email_=None, phone_=None):
+        self.id = id_
+        self.first = first_
+        self.last = last_
+        self.email = email_
+        self.phone = phone_
+
+    @staticmethod
+    def all():
+        return Contact.db
