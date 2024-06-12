e35453a283b9be9d79baddc6d5beb2fcbad3d024
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-06-12 16:48:14 +0100

    Added contacts route

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import (Flask, redirect, request, render_template)
+from contact import (Contact)
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello!</h1>"
+    return redirect("/contacts")
+
+@app.route("/contacts")
+def contscts():
+    search = request.args.get("q")
+    if search is not None:
+        contacts_set = Contact.search(search)
+    else:
+        contacts_set = Contact.all()
+    return render_template("index.html", contacts=contacts_set)
 
 app.run()
